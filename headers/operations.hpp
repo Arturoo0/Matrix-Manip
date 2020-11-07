65c9cbc6d9c3aca78653cc350401a5b306775e95
Arturoo0 <arturoportelles@gmail.com>
2020-11-07 02:51:14 -0500

    added scalar multiplication

--- headers/operations.hpp
+++ headers/operations.hpp
@@ -6,6 +6,7 @@
 namespace op {
     bool sameShape(const Matrix &m1, const Matrix&m2);
     void transpose(Matrix &m);
+    void scalarMultiplication(Matrix &m, double scalar);
     Matrix add(Matrix &m1, Matrix &m2);
     Matrix subtract(Matrix &m1, Matrix &m2);
     Matrix multiply(Matrix &m1, Matrix &m2);
